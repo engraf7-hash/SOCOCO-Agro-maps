ed75dcdf8feeee1f5e214d3da0786eaf69feee09
engraf7-hash <eng.raf7@gmail.com>
2026-09-14 21:58:52 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const APP_CACHE = "sococo-app-v19";
-const TILE_CACHE = "sococo-tiles-v19";
+const APP_CACHE = "sococo-app-v20";
+const TILE_CACHE = "sococo-tiles-v20";
 
 const APP_SHELL = [
   "./index.html",
